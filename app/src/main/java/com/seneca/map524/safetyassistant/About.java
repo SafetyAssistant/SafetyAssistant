304e76e186d6d842f8b1b86338f0d4c821cb64c6
acooc <acooc@myseneca.ca>
2016-11-17 07:29:53 -0500

    removed dependencies

--- app/src/main/java/com/seneca/map524/safetyassistant/About.java
+++ app/src/main/java/com/seneca/map524/safetyassistant/About.java
@@ -1,10 +1,8 @@
 package com.seneca.map524.safetyassistant;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
+
 import android.view.MenuItem;
 
 public class About extends AppCompatActivity {
--- app/src/main/java/com/seneca/map524/safetyassistant/Help.java
+++ app/src/main/java/com/seneca/map524/safetyassistant/Help.java
@@ -1,10 +1,7 @@
 package com.seneca.map524.safetyassistant;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class Help extends AppCompatActivity {
